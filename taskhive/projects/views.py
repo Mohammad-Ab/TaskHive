a02cc9b25e99f7b7f679a3af6591efea9679e01d
Mohammad-Ab <root.alpha1@gmail.com>
2026-06-09 11:31:53 +0330

    Adding Viewsets to The Projects and using Router Method

--- taskhive/projects/views.py
+++ taskhive/projects/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Project
+from .serializers import ProjectSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
--- taskhive/taskhive/urls.py
+++ taskhive/taskhive/urls.py
@@ -15,8 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from projects.views import ProjectViewSet
+from tasks.views import TaskViewSet
+
+router = DefaultRouter()
+
+router.register(
+    r'projects',
+    ProjectViewSet,
+    basename='project'
+)
+
+router.register(
+    r'tasks',
+    TaskViewSet,
+    basename='task'
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
