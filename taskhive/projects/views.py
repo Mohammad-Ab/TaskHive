62ad3be23a40f4cefc307d0dcdce8fbc2200e8da
Mohammad-Ab <root.alpha1@gmail.com>
2026-07-25 17:57:00 +0330

    Add JWT authentication and perform_create for projects

--- taskhive/projects/views.py
+++ taskhive/projects/views.py
@@ -8,3 +8,5 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [IsAuthenticated]
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
--- taskhive/taskhive/urls.py
+++ taskhive/taskhive/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from projects.views import ProjectViewSet
 from tasks.views import TaskViewSet
 
@@ -38,5 +42,10 @@ router.register(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('api/', include(router.urls)),
+
 ]
