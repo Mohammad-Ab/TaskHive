d2d368be80e2bff9a4e135221b36064c2bcfd326
Mohammad-Ab <root.alpha1@gmail.com>
2026-07-25 21:14:38 +0330

    Implement perform_create for project and task viewsets

--- taskhive/tasks/views.py
+++ taskhive/tasks/views.py
@@ -8,3 +8,6 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self,serializer):
+        serializer.save(created_by = self.request.user)
