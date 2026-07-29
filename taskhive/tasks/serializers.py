f4c7cef04807778374ce09553563ff44e7f98a90
Mohammad-Ab <root.alpha1@gmail.com>
2026-07-29 17:13:07 +0330

    add validation for the tasks

--- taskhive/tasks/serializers.py
+++ taskhive/tasks/serializers.py
@@ -20,6 +20,22 @@ class TaskSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id', 'created_by', 'created_at']
 
+    def validate(self, attrs):
+        project = attrs.get("project")
+        assignee = attrs.get("assignee")
+
+        if assignee is None:
+            return attrs
+
+        if assignee == project.owner:
+            return attrs
+        if project.members.filter(id=assignee.id).exists():
+            return attrs
+
+        raise serializers.ValidationError({
+            "assignee":"selected user must be the project owner or one of the project members."
+        })
+
 
 
 
